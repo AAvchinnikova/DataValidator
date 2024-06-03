7b8e14015963a2de3896bfd2fbbf09d3059b86c0
AAvchinnikova <avchinnikova_a@mail.ru>
2024-06-03 10:16:25 +0200

    project 3

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,8 +1,11 @@
 package hexlet.code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringSchema {
 
-    private boolean allFieldRequired = false;
+    private boolean statusOfRequired = false;
     private int minLength;
     private boolean statusOfMinLength = false;
     private String substring;
@@ -11,8 +14,41 @@ public class StringSchema {
 
 
     public StringSchema required() {
-        this.allFieldRequired = true;
+        this.statusOfRequired = true;
         return this;
+    }
+    public StringSchema minLength(int minLength) {
+        this.minLength = minLength;
+        this.statusOfMinLength = true;
+        return this;
+    }
+    public StringSchema contains(String subString) {
+        this.substring = subString;
+        this.statusOfSubstring = true;
+        return this;
+    }
+    public boolean isValid(String value) {
+        List<Boolean> resultCheck = new ArrayList<>();
+        if( statusOfRequired) {
+            resultCheck.add(checkRequired(value));
+        }
+        if (statusOfMinLength) {
+            resultCheck.add(checkMinLength(value));
+        }
+        if (statusOfSubstring) {
+            resultCheck.add(checkContains(value));
+        }
+        return !resultCheck.contains(false);
+    }
+
+    public boolean checkRequired(String value) {
+        return  !(value.equals(null) || value.equals(""));
+    }
+    public boolean checkMinLength(String value) {
+        return value.length() < minLength;
+    }
+    public boolean checkContains(String value) {
+        return value.contains(substring);
     }
 
 }
--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -3,8 +3,6 @@ package hexlet.code;
 public final class Validator {
 
     public void string() {
-        var value = new StringSchema();
-        var result = value.required();
-        System.out.println(result);
+
     }
 }
